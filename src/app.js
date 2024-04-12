92d193d0f173c0d0b485fa355685a2eb44aef3db
minhquang2k4 <minhwang1708@gmail.com>
2024-04-12 23:34:33 +0700

    update router

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 // console.log(path.join(__dirname, 'public'));
 
@@ -14,9 +15,28 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
+  res.send('chao mung den voi chung toi');
+});
+
+app.get('/flashcard', (req, res) => {
   res.render('flashcard');
 });
 
+app.get('/chondapan', (req, res) => {
+  res.render('chondapan');
+});
+
+app.get('/thuvien', (req, res) => {
+  res.render('thuvien');
+});
+
+app.get('/addhocphan', (req, res) => {
+  res.render('taohpmoi');
+});
+
+app.get('/hocphan', (req, res) => {
+  res.render('hpcuaban');
+});
 
 
 app.listen(port, () => {
