3192adc6811ee5bd29004262c4d7c79b20fec288
minhquang2k4 <minhwang1708@gmail.com>
2024-04-12 22:37:15 +0700

    update front-end

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+// console.log(path.join(__dirname, 'public'));
+
+// template engine
+app.set('views', `${__dirname}/resources/views`);
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+// const mongooes = require('mongoose');
+
+// const connect = async () => {
+//     try {
+//         await mongooes.connect('mongodb://localhost:27017/shortUrl');
+//         // mongodb://localhost:27017/ -> đường dẫn đến db
+//         // shortUrl -> kết nối vào db tên là shortUrl (nếu chưa có thì sẽ tạo)
+//         console.log('Database connected');
+//     } catch (error) {
+//         console.log('Database connection failed');
+//     }
+// }
+
+// module.exports = {connect};
