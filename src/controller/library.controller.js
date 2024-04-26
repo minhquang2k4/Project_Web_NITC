c06c5971a00bf993b287488bd30ca8a2318941e7
minhquang2k4 <minhwang1708@gmail.com>
2024-04-27 00:07:59 +0700

    them try catch controller

--- src/controller/library.controller.js
+++ src/controller/library.controller.js
@@ -5,16 +5,28 @@ const mongoose = require('mongoose');
 
 
 module.exports.index = async (req, res) => {
-    // get all libraries of the account
-    const libraries = await Library.find({ _id: { $in: res.locals.user.library } });
-    res.render('your-library', { libraries: libraries });
+    try {
+        // get all libraries of the account
+        const libraries = await Library.find({ _id: { $in: res.locals.user.library } });
+        res.render('your-library', { libraries: libraries });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).json({ message: 'Error' });
+    }
 }
 
 module.exports.detail = async (req, res) => {
-    const id = req.params.id;
-    const library = await Library.findOne({ _id: id });
-    const words = await Word.find({ _id: { $in: library.word } });
-    res.render('library-detail', { library: library, words: words });
+    try {
+        const id = req.params.id;
+        const library = await Library.findOne({ _id: id });
+        const words = await Word.find({ _id: { $in: library.word } });
+        res.render('library-detail', { library: library, words: words });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).json({ message: 'Error' });
+    }
 }
 
 module.exports.create = (req, res) => {
@@ -22,111 +34,148 @@ module.exports.create = (req, res) => {
 }
 
 module.exports.flashcard = async (req, res) => {
-    const id = req.params.id;
-    const library = await Library.findOne({ _id: id });
-    const words = await Word.find({ _id: { $in: library.word } });
+    try {
+        const id = req.params.id;
+        const library = await Library.findOne({ _id: id });
+        const words = await Word.find({ _id: { $in: library.word } });
+    
+        res.render('flashcard', { library: library, words: words });
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ message: 'Error' });
+    }
 
-    res.render('flashcard', { library: library, words: words });
 }
 
 module.exports.quizze = async (req, res) => {
-    const id = req.params.id;
-    const library = await Library.findOne({ _id: id });
-    const words = await Word.find({ _id: { $in: library.word } });
-
-    res.render('quizze', { library: library, words: words } );
+    try{
+        const id = req.params.id;
+        const library = await Library.findOne({ _id: id });
+        const words = await Word.find({ _id: { $in: library.word } });
+    
+        res.render('quizze', { library: library, words: words } );
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json({ message: 'Error' });
+    }
 }
 
 module.exports.createLibrary = async (req, res) => {
-    // Create a new library
-    const library = new Library({
-        title: req.body.name,
-        description: req.body.description,
-        word: []
-    });
+    try {
+         // Create a new library
+        const library = new Library({
+            title: req.body.name,
+            description: req.body.description,
+            word: []
+        });
 
-    // Save the library
-    const libraryObject = await library.save();
+        // Save the library
+        const libraryObject = await library.save();
 
-    // Find the account
-    const account = await Account.findOne({ token: req.cookies.token });
+        // Find the account
+        const account = await Account.findOne({ token: req.cookies.token });
 
-    // Add the library to the account
-    account.library.push(libraryObject._id);
-    await account.save();
+        // Add the library to the account
+        account.library.push(libraryObject._id);
+        await account.save();
 
-    // Redirect to the library
-    res.redirect('/library');
+        // Redirect to the library
+        res.redirect('/library');
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).json({ message: 'Error' });
+    }
+   
 }
 
 
 module.exports.createWord = async (req, res) => {
-    // Find the library
-    const id = req.params?.id;
-    const library = await Library.findOne({ _id: id });
+    try{
+        // Find the library
+        const id = req.params?.id;
+        const library = await Library.findOne({ _id: id });
 
-    // Create a new word
-    const word = new Word({
-        word: req.body.word,
-        meaning: req.body.meaning,
-        image: req.body.imageUrl
-    });
+        // Create a new word
+        const word = new Word({
+            word: req.body.word,
+            meaning: req.body.meaning,
+            image: req.body.imageUrl
+        });
 
-    // Save the word
-    const wordObject = await word.save();
+        // Save the word
+        const wordObject = await word.save();
 
-    // Add the word to the library
-    library.word.push(wordObject._id);
-    await library.save();
+        // Add the word to the library
+        library.word.push(wordObject._id);
+        await library.save();
 
-    // Redirect to the library
-    res.redirect('/library/detail/' + id);
+        // Redirect to the library
+        res.redirect('/library/detail/' + id);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json({ message: 'Error' });
+    }
 }
 
 
 
 module.exports.deleteLibrary = async (req, res) => {
-    // Get the library ID from the request parameters
-    const id = req.params.id;
+    try{
+        // Get the library ID from the request parameters
+        const id = req.params.id;
 
-    // Find the library
-    const library = await Library.findOne({ _id: id });
+        // Find the library
+        const library = await Library.findOne({ _id: id });
 
-    // Delete all words that belong to the library
-    await Word.deleteMany({ _id: { $in: library.word } });
+        // Delete all words that belong to the library
+        await Word.deleteMany({ _id: { $in: library.word } });
 
-    // Remove the library ID from the account
-    const account = await Account.updateOne({ 
-        _id: res.locals.user._id
-     },{
-        $pull: { library: id }
-     });
+        // Remove the library ID from the account
+        const account = await Account.updateOne({ 
+            _id: res.locals.user._id
+        },{
+            $pull: { library: id }
+        });
 
-    // Delete the library
-    await Library.deleteOne({ _id: id });
+        // Delete the library
+        await Library.deleteOne({ _id: id });
 
-    // Redirect to the library page
-    res.redirect('/library');
+        // Redirect to the library page
+        res.redirect('/library');
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json({ message: 'Error' });
+    }
 }
 
 module.exports.deleteWord = async (req, res) => {
-    // get id
-    const libID = req.params.libID;
-    const wordID = req.params.wordID;
+    try{
+        // get id
+        const libID = req.params.libID;
+        const wordID = req.params.wordID;
 
-    // Find the library
-    const library = await Library.findOne({ _id: libID });
+        // Find the library
+        const library = await Library.findOne({ _id: libID });
 
-    // Remove the word ID from the library
-    await Library.updateOne({ 
-        _id: libID
-     },{
-        $pull: { word: wordID }
-     });
+        // Remove the word ID from the library
+        await Library.updateOne({ 
+            _id: libID
+        },{
+            $pull: { word: wordID }
+        });
 
-    // Delete the word
-    await Word.deleteOne({ _id: wordID });
+        // Delete the word
+        await Word.deleteOne({ _id: wordID });
 
-    // Redirect to the library page
-    res.redirect('/library/detail/' + libID);
+        // Redirect to the library page
+        res.redirect('/library/detail/' + libID);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json({ message: 'Error' });
+    }
 }
