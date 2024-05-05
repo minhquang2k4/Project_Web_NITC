7217a25ebdc13d1036f0aac9f239716679dc63f3
hoangtv090103 <hoangtv090103@gmail.com>
2024-05-06 01:08:03 +0700

    Add ask gemini

---
+++ src/controller/chat.controller.js
@@ -0,0 +1,80 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const mongoose = require("mongoose");
+const Library = require("../model/library.model.js");
+const chatHistory = require("../model/chat.model.js");
+
+module.exports.getChat = async (req, res) => {
+	const libraryId = req.params.id;
+	const library = await Library.findById(libraryId);
+	const chat = library?.chatId
+		? await chatHistory.findById(library.chatId)
+		: null;
+	if (chat) {
+		res.json(chat.history);
+	} else {
+		res.json([]);
+	}
+};
+
+module.exports.postChat = async (req, res) => {
+	try {
+		const genAI = new GoogleGenerativeAI(
+			"AIzaSyBj9Pw3gzSD6YbDWFuZQ0spe1UiA2b3UuI",
+		);
+
+		const { msg, libraryId } = req.body;
+
+
+        const library = await Library.findById(libraryId);
+
+        // Get the chat history
+        const history = await chatHistory.findById(library.chatId);
+		const simplifiedHistory = history ? history.history.map(item => ({
+			role: item.role,
+			parts: item.parts.map(part => ({ text: part.text }))
+		})) : [];
+		
+
+		async function run() {
+			try {
+				const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+				const chat = model.startChat({
+					history: simplifiedHistory,
+				});
+				const result = await chat.sendMessage(msg);
+				const response = await result.response;
+				const text = response.text();
+
+                const newMessage = {
+                    role: "user",
+                    parts: [{ text: msg }],
+                };
+
+                const newResponse = {
+                    role: "model",
+                    parts: [{ text }],
+                };
+
+                if (history) {
+                    history.history.push(newMessage);
+                    history.history.push(newResponse);
+                    await history.save();
+                } else {
+                    const newHistory = new chatHistory({
+                        history: [newMessage, newResponse],
+                    });
+                    await newHistory.save();
+                    library.chatId = newHistory._id;
+                    await library.save();
+                }
+
+				res.json([newMessage, newResponse]);
+			} catch (error) {
+				res.status(500).send(error.message);
+			}
+		}
+		run();
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
+};
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { requireAuth, checkUser } = require('../middleware/auth.middleware.js');
 const router = express.Router();
 const controller = require('../controller/auth.controller.js');
+const chat = require('../controller/chat.controller.js');
 
 router.use('/auth', require('./auth.route.js'));
 
@@ -13,5 +14,10 @@ router.get('/', checkUser, (req, res) => {
 router.use('/library',requireAuth, require('./library.route.js'));
 
 router.get('/logout', controller.logout);
+router.get('/gemini', (req, res) => {
+  res.send("Gemini API is working!");
+})
+router.get('/gemini/:id', chat.getChat);
+router.post('/gemini', chat.postChat);
 
 module.exports = router;
