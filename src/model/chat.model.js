6c6ddd3c8f0c43cfff73c86076feb63864df6d6e
minhquang2k4 <minhwang1708@gmail.com>
2024-05-06 02:29:06 +0700

    update gemini API

---
+++ src/model/chat.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const PartSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+const MessageSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    required: true
+  },
+  parts: [PartSchema]
+});
+
+const ChatHistorySchema = new mongoose.Schema({
+  history: [MessageSchema],
+  generationConfig: {
+    maxOutputTokens: Number
+  }
+});
+
+const chatHistory = mongoose.model('ChatHistory', ChatHistorySchema);
+
+module.exports = chatHistory;
--- src/model/library.model.js
+++ src/model/library.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const wordSchema = require('./word.model.js');
+const chatHistory = require('./chat.model.js');
 
 const librarySchema = new Schema({
     title: {
@@ -15,7 +16,11 @@ const librarySchema = new Schema({
             type: Schema.Types.ObjectId,
             ref: 'Word'
         }],
-    }
+    },
+    chatId: {
+        type: Schema.Types.ObjectId,
+        ref: 'ChatHistory'
+    },
 });
 
 
